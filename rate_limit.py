580ccc4b74ea8e43bf012f99a0743e62280c5fc5
DeVerl Stokes <deverl.stokes@gmail.com>
2024-05-10 22:35:44 -0600

    Python version.

---
+++ rate_limit.py
@@ -0,0 +1,56 @@
+# Should rate limit
+# 4 cases
+#   1: key not in cache (create new entry, count = 1, return false)
+#   2: key in cache, not expired, count < max (increment count, return false)
+#   3. key in cache, not expired, count >= max (increment count, return true)
+#   4. key in cache, expired (create new entry, count = 1, return false)
+
+
+import datetime
+
+
+class Entry():
+    def __init__(self, endTime:int, count:int):
+        self.endTime = endTime
+        self.count = count
+
+
+cache = {}
+
+def unixTime() -> int:
+    return int(datetime.datetime.now().timestamp())
+
+
+def rateLimit(key:str, interval:int, maxCount:int) -> bool:
+    t = unixTime()
+    if key in cache.keys():
+        e = cache[key]
+        if t >= e.endTime:
+            e.endTime = t + interval
+            e.count = 1
+            cache[key] = e
+            return False
+        if e.count >= maxCount:
+            return True
+        else:
+            e.count += 1
+            cache[key] = e
+            return False
+    else:
+        e = Entry(t + interval, 1)
+        cache[key] = e
+        return False
+
+
+def main():
+    # False
+    print(rateLimit("device", 5, 3))
+    print(rateLimit("device", 5, 3))
+    print(rateLimit("device", 5, 3))
+
+    # True
+    print(rateLimit("device", 5, 3))
+
+
+main()
+
